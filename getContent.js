07d2e6b6b2f15abc605c5fecc015047116192cd1
Juan Villela <juan@villela.co>
2021-04-06 23:21:05 -0400

    feat: Add article and comic content extraction.

---
+++ getContent.js
@@ -0,0 +1,45 @@
+const fetch = require("node-fetch");
+const TurndownService = require("turndown");
+const { JSDOM } = require("jsdom");
+const { Readability } = require("@mozilla/readability");
+
+const turndownService = new TurndownService();
+
+/**
+ * Get Markdown version of article from url.
+ * @function
+ *
+ * @param {string} url article url
+ * @returns {string} markdown article
+ */
+const getArticle = async (url) => {
+  // get doc
+  const response = await fetch(url);
+  const data = await response.text();
+  // generate article
+  const doc = new JSDOM(data, { url });
+  const reader = new Readability(doc.window.document);
+  const article = reader.parse();
+  // convert to MD
+  const markdown = turndownService.turndown(article.content);
+
+  return markdown;
+};
+
+/**
+ * Get image file from comic url.
+ * @function
+ *
+ * @param {string} url comic url
+ * @returns {Buffer} file buffer
+ */
+const getImage = async (url) => {
+  const response = await fetch(url);
+  const data = await response.blob();
+  const buffer = new Buffer(data, "binary");
+
+  return buffer;
+};
+
+exports.getArticle = getArticle;
+exports.getImage = getImage;
