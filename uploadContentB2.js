aa37e94b02f651521d05998aa55012ab09b71dad
Juan Villela <juan@villela.co>
2021-04-10 17:07:57 -0400

    feat: Add public url return to B2 upload method.

--- uploadContentB2.js
+++ uploadContentB2.js
@@ -5,15 +5,16 @@ require("dotenv").config();
 const APP_KEY_ID = process.env.B2_APP_KEY_ID;
 const APP_KEY = process.env.B2_APP_KEY;
 const BUCKET_ID = process.env.B2_BUCKET_ID;
+const BUCKET_NAME = process.env.B2_BUCKET_NAME;
 
 /**
  * Authorize B2 bucket for upload.
  * docs: https://www.backblaze.com/b2/docs/b2_authorize_account.html
  * @function
  *
- * @returns {object} auth data
+ * @returns {object} api endpoint, auth token, and download url
  */
-const authB2 = async () => {
+const authTokens = async () => {
   const token = Buffer.from(`${APP_KEY_ID}:${APP_KEY}`).toString("base64");
 
   try {
@@ -45,18 +46,15 @@ const authB2 = async () => {
 };
 
 /**
- * Upload file to B2 bucket.
- * docs: https://www.backblaze.com/b2/docs/b2_upload_file.html
+ * Get B2 endpoint for upload.
+ * docs: https://www.backblaze.com/b2/docs/b2_get_upload_url.html
  * @function
  *
- * @param {Buffer} data file buffer
- * @param {string} name file name with extension
- * @param {string} [type] file type
- * @returns {void}
+ * @returns {object} upload endpoint, auth token, and download url
  */
-const uploadToB2 = async (data, name, type) => {
+const getUploadUrl = async () => {
   try {
-    const authData = await authB2();
+    const authData = await authTokens();
     const response = await fetch(
       `${authData.apiUrl}/b2api/v1/b2_get_upload_url`,
       {
@@ -77,12 +75,36 @@ const uploadToB2 = async (data, name, type) => {
 
     const endpoint = results.uploadUrl;
     const authToken = results.authorizationToken;
+
+    return {
+      endpoint,
+      authToken,
+      downloadUrl: authData.downloadUrl,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Upload file to B2 bucket.
+ * docs: https://www.backblaze.com/b2/docs/b2_upload_file.html
+ * @function
+ *
+ * @param {Buffer} data file buffer
+ * @param {string} name file name with extension
+ * @param {string} [type] file type
+ * @returns {string} file public url
+ */
+const uploadToB2 = async (data, name, type) => {
+  try {
+    const authData = await getUploadUrl();
     const hash = createHash("sha1").update(data).digest("hex");
 
-    const uploadResp = await fetch(endpoint, {
+    const response = await fetch(authData.endpoint, {
       method: "POST",
       headers: {
-        Authorization: authToken,
+        Authorization: authData.authToken,
         "X-Bz-File-Name": name,
         "Content-Type": type || "b2/x-auto",
         "Content-Length": data.length,
@@ -91,17 +113,16 @@ const uploadToB2 = async (data, name, type) => {
       },
       body: data,
     });
-    const uploadStatus = await uploadResp.json();
+    const results = await response.json();
 
-    if (uploadResp.status !== 200) {
-      throw new Error(`${uploadStatus.status}: ${uploadStatus.code}`);
+    if (response.status !== 200) {
+      throw new Error(`${results.status}: ${results.code}`);
     }
 
-    return uploadStatus;
+    return `${authData.downloadUrl}/file/${BUCKET_NAME}/${results.fileName}`;
   } catch (error) {
     console.error(error);
   }
 };
 
-exports.authB2 = authB2;
 exports.uploadToB2 = uploadToB2;
