a2e49c6a800b211aa87f954799510118e9d791a8
Juan Villela <juan@villela.co>
2021-04-12 15:54:41 -0400

    fix: Update Webpack config for cleaner output.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-const nodeExternals = require('webpack-node-externals');
 const { join } = require('path');
 
 const mode = process.env.NODE_ENV || 'production';
+const name = mode === 'development' ? `index.${mode}.js` : 'index.js';
 
 module.exports = {
+  entry: join(__dirname, 'src/index.ts'),
+  context: __dirname,
   output: {
-    filename: `worker.${mode}.js`,
+    filename: name,
     path: join(__dirname, 'dist'),
   },
   mode,
   resolve: {
     extensions: ['.ts', '.js'],
-    plugins: [],
+    modules: [__dirname, 'node_modules'],
   },
-  externals: [ nodeExternals() ],
+  target: 'node',
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true,
+        test: /\.ts/,
+        use: {
+          loader: 'ts-loader',
         },
+        exclude: /node_modules/,
       },
     ],
+  },
+  externals: {
+    canvas: 'commonjs canvas',
   },
 };
