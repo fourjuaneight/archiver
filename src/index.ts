a93644ab666243cd4c9273f215aabcec3e40ae24
Juan Villela <juan@villela.co>
2021-04-17 16:38:34 -0400

    feat: Abstract main archiver method.
    - Add helper method to loop over records and archive then update one at a time.
    - Minor syntax fixes and cleanup.

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,43 @@ import uploadToB2 from './addFiletoRecord';
 import {
   baseQueries,
   getBookmarksWithOffset,
-  updateBookmarks,
+  getRecords,
+  updateBookmark,
 } from './getBookmarks';
 
 import { Record } from './types';
 
+/**
+ * Archive media to B2 and update record on Airtable.
+ * @function
+ *
+ * @param {string} list table name
+ * @param {Record[]} records archive to archive and update
+ * @return {Promise<void>}
+ */
+const archiveRecord = async (
+  list: string,
+  records: Record[]
+): Promise<void> => {
+  for (let record of records) {
+    const updatedRecord = await addFiletoRecord(list, record);
+    await updateBookmark(list, updatedRecord);
+  }
+};
+
 (async () => {
-  const bookmarksList = Object.keys(baseQueries.Bookmarks);
-
   try {
-    // Get latest bookmarks
-    for (const list of bookmarksList) {
-      await getBookmarksWithOffset('Bookmarks', list);
-    }
+    const bookmarks = await getRecords();
 
-    // Update bookmarks missing file archive
-    for (const list of bookmarksList) {
-      // update only those that do not have a file
-      const cleanRecords = baseQueries.Bookmarks[list].filter(
-        record => !record.fields.file
+    // update bookmarks missing file archive
+    for (const list of Object.keys(bookmarks)) {
+      // update only those that do not have a file archive
+      const cleanRecords = bookmarks[list].filter(
+        record => !record.fields.archive
       );
 
-      if (cleanList > 0) {
-        const updatedRecords = await addFiletoRecord(list, cleanRecords);
-
-        await updateBookmarks(list, updatedRecords);
+      if (cleanRecords.length > 0) {
+        await archiveRecord(list, cleanRecords);
       } else {
         console.info(`No records to update in ${list}.`);
       }
