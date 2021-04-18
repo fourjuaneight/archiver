2db70ce58f5e2b84163935f057424dcea70950dd
Juan Villela <juan@villela.co>
2021-04-18 15:06:57 -0400

    feat: Add tweets archive script.
    - Renamed bookmarks archiving script to easier distinction.

---
+++ src/archive-tweets.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+
+import latest from './helpers/latestTweets';
+import uploadTweets from './helpers/uploadTweets';
+
+import type { LatestTweetFmt } from './models/twitter';
+
+// Upload latest tweets to Airtable base.
+(async () => {
+  try {
+    // get formatted tweets
+    const tweets: LatestTweetFmt[] | null = await latest();
+
+    // upload each individually
+    if (tweets && tweets.length > 0) {
+      for (const tweet of tweets) {
+        try {
+          const upload = await uploadTweets(tweet);
+
+          // post Airtable ID to console when uploaded
+          console.info(chalk.green('[SUCCESS]'), `Tweet uploaded: ${upload}.`);
+        } catch (error) {
+          console.error(chalk.red('[ERROR]'), error);
+          throw new Error(error);
+        }
+      }
+    } else {
+      console.info(chalk.yellow('[INFO]'), 'No new tweets to upload.');
+    }
+  } catch (error) {
+    console.error(chalk.red('[ERROR]'), error);
+    throw new Error(error);
+  }
+})();
