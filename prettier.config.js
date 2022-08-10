e5ff58475c136abfaa62ee29e39e15dfb36ad644
Juan Villela <juan@villela.co>
2022-08-10 10:45:04 -0400

    feat: Add import order config to prettier.

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,18 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSpacing: true,
   endOfLine: 'auto',
+  importOrder: [
+    'react', // React itself
+    '<THIRD_PARTY_MODULES>', // node_modules
+    '^(cplibrary|cpicons|cputil|cpclient|@cpclient|cpshots).*$', // Workspace packages
+    'components/.*$', // React Components
+    '^(constants|data|hooks|util|utils)/.*$', // Various helpers
+    '^(\\.|\\.\\.)/(.(?!.(css|scss)))*$', // Any local imports that AREN'T styles.
+    '\\.(css|scss)$', // Styles
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
   overrides: [
     {
       files: '*.json',
@@ -18,4 +30,5 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'es5',
   useTabs: false,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
 };
